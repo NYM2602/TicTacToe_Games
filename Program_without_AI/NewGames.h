f8932bc7cb50c70168996df1413c54312126fce4
Noura <nourata20066@gmail.com>
2024-12-04 13:21:22 +0200

    add sections for each game

--- Program_without_AI/NewGames.h
+++ Program_without_AI/NewGames.h
@@ -1,6 +1,46 @@
 #ifndef NEWGAMES_H
 #define NEWGAMES_H
 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+//Game 1
+
+
+
+
+//Game 2
+
+
+
+
+//Game 3
+
+
+
+
+//Game 4
+
+
+
+//Game 5
+
+
+
+
+//Game 6
+
+
+
+
+
+//Game 8
+
+
+
+
+//Game 9
 
 
 
